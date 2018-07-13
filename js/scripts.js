653a256fde423e29ea63a8fefa8443e798c4f2fe
DGichuru <dangichuru77@gmail.com>
2018-07-13 13:43:18 +0300

    Divisibiltiy

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
 $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
-        event.preventDefault();
-        var result = pingpong
-        
-        $(".Pingpong").text(number)
+        $("#results").empty();
 
-        $("#result").show
+        var number = parseInt($("input#number").val());
+        var no = Number(number);
+        no.forEach(function(aftermath) {
+            $("#results").append("<li>" + aftermath + "</li>");
+            event.preventDefault();
+        });
+
+        
     });
 });
 
 // jQuery user interface logic here
  
-var output = function(number) {
-    var array = [];
-    for (var index = 1; index <= number; index++) {
-        if ((index % 15 ) === 0)
-    }
-}
+    
+    var Number = function(num) {
+        var no = [];
+        for(index=1; index<= num; index+=1) {
+
+        if ((num % 3 === 0) && (num % 5 === 0)) {
+          no.push ("pingpong");
+    
+        }else if(num % 5 === 0) {
+           no.push ( "pong");
+
+        }else if (num % 3 === 0) {
+          no.push("ping");
+        } else 
+            no.push (num);
+        
+       }   
+        console.log(no);
+        return no;
+      };
+
+//Business logic
