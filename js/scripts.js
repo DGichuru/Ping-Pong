2185efafe6c68a0e32a4604c13f2876a71d3d54d
DGichuru <dangichuru77@gmail.com>
2018-07-13 10:33:37 +0300

    divisibilty of 3-5-15

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
-$(documents).ready(function() {
+$(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
         event.preventDefault();
-        var result = pingpong(number);
-        $("result").show();
+        var result = pingpong
+        
+        $(".Pingpong").text(number)
+
+        $("#result").show
     });
 });
 
 // jQuery user interface logic here
  
-var pingpong = function(number) {
-    if(number % 3 === 0) {
-        return ping;
-    } else {
-        return false;
-    };
-};
+var output = function(number) {
+    var array = [];
+    for (var index = 1; index <= number; index++) {
+        if ((index % 15 ) === 0)
+    }
+}
